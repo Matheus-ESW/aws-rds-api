1a898d76f3ba81030c3caa36604f17b4d2f7ce66
Matheus-ESW <matheussoaresramos@gmail.com>
2025-11-08 09:54:52 -0300

    Cria arquivo usando pydanticgit add .

--- src/fetch_1.py
+++ src/fetch_1.py
@@ -22,16 +22,26 @@ headers = {
 session = Session()
 session.headers.update(headers)
 
-response = session.get(url=url, params=parameters)
-data = json.loads(response.text)
+def consultar_cotacao_bitcoin():
 
-## print(json.dumps(data, indent=4))
+    try:
+        response = session.get(url=url, params=parameters)
+        data = json.loads(response.text)
 
-btc_data = data["data"]["BTC"]
-brl_quote = btc_data["quote"]["BRL"]
+        if 'data' in data and 'BTC' in data['data']:
 
-pprint(brl_quote)
-pprint(brl_quote["price"])
-pprint(brl_quote["last_updated"])
-print(brl_quote["volume_24h"])
-print(brl_quote["market_cap"])
+            btc_data = data["data"]["BTC"]
+            brl_quote = btc_data["quote"]["BRL"]
+
+            print(f"Última cotação do Bitcoin: ${brl_quote["price"]:.2f} BRL")
+            print(f"Volume 24H: ${brl_quote["volume_24h"]:.2f} BRL")
+            print(f"Market Cap: ${brl_quote["market_cap"]:.2f} BRL")
+            print(f"Última atualização: {brl_quote["last_updated"]}")
+        else:
+            print("Erro ao obter a cotação do Bitcoin: ", data['status'].get('error_message', 'Erro desconhecido'))
+
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(f"Erro na requisição: {e}")
+
+
+consultar_cotacao_bitcoin()
