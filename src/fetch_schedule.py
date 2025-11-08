f3a7b6b5a2b1d3b9b1299da81ddf6189942fd814
Matheus-ESW <matheussoaresramos@gmail.com>
2025-11-08 11:28:29 -0300

    Adiciona função persistir dados em csvgit add .

--- src/fetch_schedule.py
+++ src/fetch_schedule.py
@@ -3,6 +3,9 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 from dotenv import load_dotenv
 import json
 import os
+import schedule
+import time
+import csv
 
 load_dotenv()
 
@@ -36,11 +39,33 @@ def consultar_cotacao_bitcoin():
             print(f"Volume 24H: ${brl_quote["volume_24h"]:.2f} BRL")
             print(f"Market Cap: ${brl_quote["market_cap"]:.2f} BRL")
             print(f"Última atualização: {brl_quote["last_updated"]}")
+
+            csv_path = "csv_data/bitcoin.csv"
+
+            # Verifica se já existe (para decidir se escreve o cabeçalho)
+            arquivo_existe = os.path.exists(csv_path)
+
+            with open(csv_path, "a", newline="", encoding="utf-8") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=brl_quote.keys())
+
+                # Se o arquivo não existia, escreve o cabeçalho
+                if not arquivo_existe:
+                    writer.writeheader()
+
+                # Escreve uma linha com os dados da cotação
+                writer.writerow(brl_quote)
+
+            print("Linha adicionada ao CSV!")
+
         else:
             print("Erro ao obter a cotação do Bitcoin: ", data['status'].get('error_message', 'Erro desconhecido'))
 
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         print(f"Erro na requisição: {e}")
 
+schedule.every(15).seconds.do(consultar_cotacao_bitcoin)
 
-consultar_cotacao_bitcoin()
+print("Iniciando o agendamento para consultar a API a cada 15 segundos..")
+while True:
+    schedule.run_pending()
+    time.sleep(1)
